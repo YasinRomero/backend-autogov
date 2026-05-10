c3c0ea73bbf35834441467f12139052d640f7b70
Aylin-28 <U23231775@utp.edu.pe>
2026-05-10 12:50:19 -0500

    Middleware - validacion de token

--- app/core/security.py
+++ app/core/security.py
@@ -1,4 +1,4 @@
-from jose import jwt
+from jose import JWTError, jwt
 from datetime import datetime, timedelta, timezone
 
 SECRET_KEY = "llaveprivadaparatoken123wa"
@@ -11,3 +11,18 @@ def create_token(data : dict):
 
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITM)
+
+
+
+## Usen este metodo para validar tokens
+def verify_token(token: str):
+    try:
+        payload = jwt.decode(
+            token,
+            SECRET_KEY,
+            algorithms=[ALGORITM]
+        )
+        return payload
+
+    except JWTError:
+        return None

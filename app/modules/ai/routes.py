e752e150a3c20ae625f49e80cbcfb19a81cc8a3e
Aylin-28 <U23231775@utp.edu.pe>
2026-05-12 22:13:20 -0500

    Historial route + Middleware implementado

--- app/modules/ai/routes.py
+++ app/modules/ai/routes.py
@@ -1,9 +1,12 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 
 from app.modules.ai.schemas import AskRequest, AskResponse
 from app.modules.ai.service import ask_ai, clear_ai_service
+from app.modules.auth.dependencies import get_current_user
 
-router = APIRouter()
+router = APIRouter(
+    dependencies=[Depends(get_current_user)]
+)
 
 @router.post("/ask", response_model=AskResponse)
 def aiask(data: AskRequest):
@@ -12,3 +15,8 @@ def aiask(data: AskRequest):
 @router.post("/clearfiles", response_model=dict)
 def clear_ai_system():
     return clear_ai_service()
+
+@router.post("/historial", response_model=historialResponse)
+def get_historial():
+    return get_historial()
+

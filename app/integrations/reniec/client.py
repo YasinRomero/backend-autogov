150064895c5329f8653ec0fcb5df0fb709b12c5b
LeonPastorCarlosDaniel <U23211073@utp.edu.pe>
2026-05-13 19:23:33 -0500

    agrego la validacion de la existencia del dni con datos de RENIEC

--- app/integrations/reniec/client.py
+++ app/integrations/reniec/client.py
@@ -1,1 +1,26 @@
-## CLASE PARA QUE IMPLEMENTES LOGICA DE LA API DE RENIEC
+import requests
+
+class ReniecClient:
+
+    TOKEN = "2e2d06338eddc98894bf69f9268b96554060d628ed34416ea0c64b9358a0ce8d"
+
+    BASE_URL = "https://api.apis.net.pe/v2/reniec/dni"
+
+    def validate_dni(self, dni: str):
+
+        response = requests.get(
+            f"{self.BASE_URL}?numero={dni}",
+            headers={
+                "Authorization": f"Bearer {self.TOKEN}"
+            }
+        )
+
+        if response.status_code != 200:
+            return None
+
+        data = response.json()
+
+        if not data:
+            return None
+
+        return data

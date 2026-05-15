35d769f79ed6e166978aff5dda0fe98c2155ba2f
LeonPastorCarlosDaniel <U23211073@utp.edu.pe>
2026-05-14 20:18:33 -0500

    Se añade la funcion clean_text para dar formato al nombre completo ingresado por el usuario en el formulario de registro y al nombre completo retornado por la api de reniec.

--- app/modules/auth/service.py
+++ app/modules/auth/service.py
@@ -1,13 +1,41 @@
-from app.modules.auth.schemas import DNIRegisterRequest, ImmigrationCardRegisterRequest, LoginRequest
+from fastapi import HTTPException
+from sqlalchemy.orm import Session
+import re
+import unicodedata
 
-def login_with_dni(data : LoginRequest):
-    ## COMPLETAR LOGICA
-    return null
+from app.modules.auth.schemas import (
+    DNIRegisterRequest,
+    ImmigrationCardRegisterRequest,
+    LoginRequest
+)
 
-def register_with_dni(data : DNIRegisterRequest):
-    ## COMPLETAR LOGICA
-    return null
+from app.models.user import User
 
-def register_with_immigrationcard(data : ImmigrationCardRegisterRequest):
-    ## COMPLETAR LOGICA
-    return null
+from app.db.session import SessionLocal
+
+from app.integrations.reniec.client import ReniecClient
+
+from app.core.security import (
+    create_token,
+    hash_password,
+    verify_password_hash
+)
+
+
+# =========================================
+# UTILIDADES
+# =========================================
+
+# Este funciona en los nombres del form y de RENIEC. Les da un formato para compararlos y ver si coinciden
+def clean_text(text: str):
+
+    return (
+        unicodedata.normalize("NFD", text)
+        .encode("ascii", "ignore")
+        .decode("utf-8")
+        .replace(",", "")
+        .lower()
+        .strip()
+    )
+
+

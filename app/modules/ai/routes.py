caf87e44a56cfaa84b3b0bc82d1240baef1d15c9
LitanoMarceloAngeloAlexis <U23210902@utp.edu.pe>
2026-05-17 22:08:53 -0500

    END | LLM con historial y entrenamiento previo terminado

--- app/modules/ai/routes.py
+++ app/modules/ai/routes.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 
 from app.db.session import get_db
 from app.modules.ai.schemas import AskRequest, AskResponse, HistorialResponse
-from app.modules.ai.service import ask_ai, clear_ai_service, get_chat_history, get_fileList
+from app.modules.ai.service import ask_ai, clear_ai_service, get_chat_history, get_fileList, get_user_chats_list
 from app.modules.auth.dependencies import get_current_user
 
 router = APIRouter(
@@ -14,14 +14,25 @@ router = APIRouter(
 def aiask(data: AskRequest, db: Session = Depends(get_db), current_user = Depends(get_current_user)):
     return ask_ai(data=data, db=db, user_id=current_user["user_id"])
 
+
+@router.get("/history/{chat_id}", response_model=HistorialResponse)
+async def fetch_history(chat_id: str, db: Session = Depends(get_db)):
+    return get_chat_history(chat_id, db)
+
+@router.get("/chats", response_model=list)
+def list_user_conversations(db: Session = Depends(get_db), current_user = Depends(get_current_user)):
+    return get_user_chats_list(db, user_id=current_user["user_id"] )
+
+
+
+
+
+## PROXIMAMENTE DEPRECADOS
 @router.post("/clearfiles", response_model=dict)
 def clear_ai_system():
     return clear_ai_service()
 
-@router.get("/history/{chat_id}", response_model=HistorialResponse)
-async def fetch_history(chat_id: str):
-    return get_chat_history(chat_id)
-
+## PROXIMAMENTE DEPRECADOS
 @router.get("/files", response_model=list)
 def list_ai_files():
     try:

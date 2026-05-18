ccb30615d6a46eb0e6ec2f5e864af89937710450
YasinRomero <U23220351@utp.edu.pe>
2026-05-18 01:08:44 -0500

    test: Agregar nuevas funciones del core

--- test/test_auth_utils.py
+++ test/test_auth_utils.py
@@ -1,4 +1,5 @@
 from app.modules.auth.service import clean_text
+from app.core.security import hash_password, verify_password_hash
 
 
 def test_clean_text_greeting_with_spaces_and_special_characters():
@@ -29,3 +30,17 @@ def test_clean_text_identifier_with_hyphens_and_symbols():
     text_to_clean = "usuario_123-test!"
     expected_result = "usuario_123-test!"
     assert clean_text(text_to_clean) == expected_result
+
+
+def test_hash_password():
+    password = "Contraseñadejuanxd1"
+    hashed_password = hash_password(password)
+    assert hashed_password is not None
+    assert type(hashed_password) is str
+
+
+def test_verify_password_hash():
+    password = "Contraseñadejuanxd1"
+    hashed_password = hash_password(password)
+    assert verify_password_hash(password, hashed_password) is True
+    assert verify_password_hash("Contraseñadejuanxd2", hashed_password) is False

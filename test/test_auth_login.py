e3bbdaf8009b211ccbe268ab33fb2bcaee969e0a
Julio Gabriel Yanac Gutierrez <juliogabriel2@gmail.com>
2026-06-01 19:34:52 -0500

    Test de login: prueba de rechazo por contraseña incorrecta

--- test/test_auth_login.py
+++ test/test_auth_login.py
@@ -19,7 +19,7 @@ def test_login_with_dni_success(mock_create_token, mock_verify_password_hash, mo
     mock_user.user_type = "ciudadano"
     mock_user.password = "hashed_password_mock"
 
-    # Configuración deretornos de los mocks
+    # Configuración de los retornos de los mocks
     mock_session.query.return_value.filter.return_value.first.return_value = mock_user
     mock_verify_password_hash.return_value = True
     mock_create_token.return_value = "token_de_prueba_jwt"
@@ -31,5 +31,25 @@ def test_login_with_dni_success(mock_create_token, mock_verify_password_hash, mo
     assert response == {"access_token": "token_de_prueba_jwt", "token_type": "bearer"}
     mock_session.query.return_value.filter.return_value.first.assert_called_once()
     mock_verify_password_hash.assert_called_once_with("PasswordValid123", "hashed_password_mock")
+    assert mock_session.close.call_count == 1
+
+@patch("app.modules.auth.service.verify_password_hash")
+def test_login_with_dni_invalid_password(mock_verify_password_hash, mock_session):
+    login_data = LoginRequest(
+        document="05302528",
+        password="WrongPassword1"
+    )
+
+    mock_user = MagicMock()
+    mock_user.password = "hashed_password_mock"
+
+    mock_session.query.return_value.filter.return_value.first.return_value = mock_user
+    mock_verify_password_hash.return_value = False
+
+    with pytest.raises(HTTPException) as exc_info:
+        login_with_dni(login_data)
+
+    assert exc_info.value.status_code == 401
+    assert exc_info.value.detail == "Credenciales inválidas"
     assert mock_session.close.call_count == 1
 

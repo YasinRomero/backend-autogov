042ff7678404f38bb74069475856928fc08cd6cc
LitanoMarceloAngeloAlexis <U23210902@utp.edu.pe>
2026-06-23 21:26:08 -0500

    CORS

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ app = FastAPI(
 
 origins = [
     "http://localhost:4200",
-    "https://front.com",
+    "https://grupo03-tramitesmunicipalesautomaticos-1kxg.onrender.com",
 ]
 
 app.add_middleware(

b49b193301e0a498eb25fe93d59d29b54b739486
YasinRomero <U23220351@utp.edu.pe>
2026-06-30 07:50:59 -0500

    chore: Solucion a error de barra en la url

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ app = FastAPI(
 
 origins = [
     "http://localhost:4200",
-    "https://tramitesmunicipalesweb.onrender.com/",
+    "https://tramitesmunicipalesweb.onrender.com",
 ]
 
 app.add_middleware(
